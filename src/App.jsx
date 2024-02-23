75059cb292ab4af0c72397c720411ccdf23a7c2c
genbaku <140975610+genbaku@users.noreply.github.com>
2024-02-24 00:33:49 +0100

    MAYBE

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,49 @@
-
-import { Profile } from "./components/Profile/Profile"
-import { FriendList } from "./components/FriendList/FriendList"
-import { TransactionHistory } from "./components/TransactionHistory/TransactionHistory"
-import userData from "./data/userData.json"
-import friends from "./data/friends.json"
-import transactions from "./data/transactions.json"
+import ContactForm from "./components/ContactForm/ContactForm"
+import SearchBox from "./components/SearchBox/SearchBox"
+import ContactList from "./components/ContactList/ContactList"
+import { useState, useEffect } from "react"
 
 export const App = () => {
+
+  const [contacts, setContacts] = useState(() => {
+    const storedContacts = localStorage.getItem('contacts');
+    return storedContacts ? JSON.parse(storedContacts) : [];
+  });
+
+  const [searchWord, setSearchWord] = useState('');
+
+  useEffect(() => {
+    localStorage.setItem('contacts', JSON.stringify(contacts));
+  }, [contacts]);
+
+  const handleSearch = (event) => {
+    setSearchWord(event.target.value);
+  };
+
+  const filteredContacts = contacts.filter(contact =>
+    contact.name.toLowerCase().includes(searchWord.toLowerCase())
+  );
+
+  const addContact = (newContact) => {
+    setContacts([...contacts, newContact]);
+  };
+
+  const deleteContact = (id) => {
+    const deletedContactRemoved = contacts.filter(contact => contact.id !== id);
+    setContacts(deletedContactRemoved);
+  };
+
+  
+
   return (
     <>
-      <Profile
-        name={userData.username}
-        tag={userData.tag}
-        location={userData.location}
-        image={userData.avatar}
-        stats={userData.stats}
-      />
-      <hr />
-      <FriendList friends={friends} />
-      <hr />
-      <TransactionHistory items={transactions} />
+      <h1>Phonebook</h1>
+      
+      <ContactForm onSubmit={addContact}/>
+
+      <SearchBox value={searchWord} onChange={handleSearch} />
+
+      <ContactList contacts={filteredContacts} onDelete={deleteContact}/>
     </>
   )
 }
